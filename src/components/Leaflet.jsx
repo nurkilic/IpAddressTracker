2df8a5f918f47d9bf038ed45876043209815badf
Nur <rumeysanurkilic@gmail.com>
2024-09-01 02:25:52 +0300

    location icon has been changed

--- src/components/Leaflet.jsx
+++ src/components/Leaflet.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useRef } from "react";
-
-const Leaflet = ({ data}) => {
+const Leaflet = ({ data }) => {
   const mapRef = useRef(null);
 
   const createleaflet = () => {
     if (mapRef.current) {
       mapRef.current.remove();
     }
+
     mapRef.current = L.map("map").setView(
       [data.location.lat, data.location.lng],
-      13
+      15
     );
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       maxZoom: 19,
@@ -17,7 +17,14 @@ const Leaflet = ({ data}) => {
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(mapRef.current);
 
-    L.marker([data.location.lat, data.location.lng]).addTo(mapRef.current);
+    var iconLocation = L.icon({
+      iconUrl: "/icon-location.svg",
+      iconSize: [50, 66],
+      iconAnchor: [25, 66], // point of the icon which will correspond to marker's location
+    });
+    L.marker([data.location.lat, data.location.lng], {
+      icon: iconLocation,
+    }).addTo(mapRef.current);
   };
   useEffect(() => {
     createleaflet();
